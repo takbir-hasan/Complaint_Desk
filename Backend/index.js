104ad1938f804e2589bcca82ff12c120a7a3aace
takbir-hasan <takbirhasan274@gmail.com>
2024-09-20 23:43:31 +0600

    backend setup

---
+++ Backend/index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+dotenv.config();
+
+const app = express()
+const port = process.env.PORT || 3000
+const URI = process.env.MongoDB
+
+try {
+  mongoose.connect(URI)
+  console.log("Connected to MongoDB");
+  
+} catch (error) {
+  console.log("Error",error);
+  
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening http://www.localhost:${port}`)
+})
