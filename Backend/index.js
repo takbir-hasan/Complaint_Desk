ee5d063f829d53f15cfc59636772af01c5451ce2
takbir-hasan <takbirhasan274@gmail.com>
2024-09-21 02:02:29 +0600

    mondodb

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose';
+
 dotenv.config();
+
 const app = express()
 const port = process.env.PORT || 3000
 const URI = process.env.MongoDB
+
+
 try {
   mongoose.connect(URI)
   console.log("Connected to MongoDB");
@@ -13,9 +17,14 @@ try {
   console.log("Error",error);
   
 }
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening http://www.localhost:${port}`)
 })
