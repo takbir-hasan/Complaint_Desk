3c68baf905b267885696e04b0579bf712d4a4fb6
mdsaniulbasirsaz <saniulsaj@gmail.com>
2024-09-24 23:21:31 +0600

    wow dashboard complete onk kosto kore korisi

--- Backend/route/complaint.route.js
+++ Backend/route/complaint.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 // import {complaint} from "../controller/complaint.controller.js";
-import { complaint, getComplaintsByDept,discardComplaint, getDiscardedComplaintsByDept} from "../controller/complaint.controller.js"; 
+import { complaint, getComplaintsByDept,discardComplaint, getDiscardedComplaintsByDept, markComplaintAsPending, markComplaintAsSolved, getPendingComplaintBycdept} from "../controller/complaint.controller.js"; 
 
 const router = express.Router();
 
@@ -8,5 +8,9 @@ router.post("/",complaint)
 router.get('/:cdept', getComplaintsByDept);
 router.get('/discarded/:cdept', getDiscardedComplaintsByDept);
 router.put('/:id', discardComplaint);
+router.put('/pending/:id', markComplaintAsPending);
+// router.get('/pending/:id', getPendingComplaints)
+router.get('/pendingsolve/:cdept', getPendingComplaintBycdept);
+router.put('/solved/:id',markComplaintAsSolved);
 
 export default router;
