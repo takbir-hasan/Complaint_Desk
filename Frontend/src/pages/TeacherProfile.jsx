0e63457bae36419e91c1dfd5e7365ee793118188
mdsaniulbasirsaz <saniulsaj@gmail.com>
2024-10-23 01:04:28 +0600

    add automatic refresh

--- Frontend/src/pages/TeacherProfile.jsx
+++ Frontend/src/pages/TeacherProfile.jsx
@@ -153,6 +153,18 @@ const handleTeacherListClick = () => {
     window.location.href = '/login';
   }
 
+  //Refresh function
+  const [hasRefreshed, setHasRefreshed] = useState(false);
+  useEffect(() => {
+    if (!hasRefreshed) {
+        setTimeout(() => {
+            window.location.reload();
+            setHasRefreshed(true);  
+        }, 1000); 
+    }
+  }, [hasRefreshed]); 
+
+
   return (
     <>
       <Helmet>
